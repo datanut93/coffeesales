3aac1f7702419842e8eaf9c8cffe971200d40b5c
datanut93 <64046589+datanut93@users.noreply.github.com>
2023-12-29 15:28:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,12 +72,12 @@ elif option == 'Product Insights':
 
     # Top revenue product
     st.subheader("Top Revenue Product")
-    top_revenue_product = data.groupby('product_detail')['total_sales'].sum()#.sort_values(ascending=False).head(1)
+    top_revenue_product = data.groupby('product_category')['total_sales'].sum().sort_values(ascending=True)
     st.bar_chart(top_revenue_product)
 
     # Top selling qty product
     st.subheader("Top Selling Quantity Product")
-    top_qty_product = data.groupby('product_detail')['transaction_qty'].sum().sort_values(ascending=False).head(1)
+    top_qty_product = data.groupby('product_category')['transaction_qty'].sum().sort_values(ascending=True)
     st.bar_chart(top_qty_product)
 
     # Profit per unit of products (assuming profit data is available or can be calculated)
